67e5601d114ec0942fe440955853aa2fd7ff0c90
BrunoNaves07 <bruno.naves@estudante.ufla.br>
2020-08-18 16:55:28 -0300

    Melhoria pesquisa vendas

--- src/resources/views/vendas.blade.php
+++ src/resources/views/vendas.blade.php
@@ -50,6 +50,15 @@
         </table>
     </div>
 </div>
-
+<script>
+    $(document).ready(function () {
+        $("#pesquisar").on("keyup", function () {
+            var value = $(this).val().toLowerCase();
+            $("#tabela tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+    });
+</script>
 
 @endsection
