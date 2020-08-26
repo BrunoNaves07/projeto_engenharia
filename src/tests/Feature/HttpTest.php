92afe047f235dc13de255d09b30ec8c3178667ee
BrunoNaves07 <bruno.naves@estudante.ufla.br>
2020-08-26 13:58:26 -0300

    Teste HTTP e Verificação no Banco de Dados

--- src/tests/Feature/HttpTest.php
+++ src/tests/Feature/HttpTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Http\Request;
+use App\Http\Controllers\AutorController;
+use App\Models\Autor;
+use App\Http\Requests\AutorRequest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -29,7 +33,7 @@ class HttpTest extends TestCase
     // Clientes
     public function test_acesso_clientes()
     {
-        $this->get('/clientes')->assertStatus(302);
+        $this->get('/clientes')->assertStatus(200);
     }
 
     // Livros
@@ -60,6 +64,59 @@ class HttpTest extends TestCase
     public function test_acesso_vendas()
     {
         $this->get('/vendas')->assertStatus(302);
+    }
+
+    /**
+     * Verifica se existe determinado dado
+     * na tabela
+     */
+
+    // Cliente
+    public function test_verifica_existe_cliente()
+    {
+        $this->assertDatabaseHas('clientes', [
+            'nome' => 'Bruno Carlos de Mesquita Naves',
+        ]);
+    }
+
+    // Usuario
+    public function test_verifica_existe_usuario()
+    {
+        $this->assertDatabaseHas('users', [
+            'nome' => 'Bruno Carlos de Mesquita Naves',
+        ]);
+    }
+
+    // Funcionário
+    public function test_verifica_existe_funcionario()
+    {
+        $this->assertDatabaseHas('funcionarios', [
+            'logradouro' => 'Rua Professora Maria Madalena',
+        ]);
+    }
+
+    // Autor
+    public function test_verifica_existe_autor()
+    {
+        $this->assertDatabaseHas('autores', [
+            'nome' => 'Pedro Alvares Cabral',
+        ]);
+    }
+
+    // Editora
+    public function test_verifica_existe_editoras()
+    {
+        $this->assertDatabaseHas('editoras', [
+            'cnpj' => '07.105.360/0001-50',
+        ]);
+    }
+
+    // Livro
+    public function test_verifica_existe_livro()
+    {
+        $this->assertDatabaseHas('livros', [
+            'titulo' => 'Como Descobri o Brasil',
+        ]);
     }
 
 }
